a7e2f30cf6ddd038af94a42b234dc3d57c51d834
Ayanacyan <amitpacl@gmail.com>
2026-01-14 19:30:00 -0600

    Check age

---
+++ Check age.py
@@ -0,0 +1,13 @@
+print("What is your age?")
+age=int(input("Enter your current age:"))
+
+if (10<=age<=20):
+    print("You are eligible for this class")
+   
+else:
+    if(10<=age<=23):
+        print("You are a little overage for this class")
+    elif(8<=age<=20):
+        print("You are a little underage for this class")
+    else:
+        print("You are way younger or older than the age gap allowed")
